69b6d9bdcb12550dca7f0dd16423ede3d4d6a9ac
Alexandre Paes Rigão <rigao.alexandre@gmail.com>
2025-04-26 00:46:57 -0300

    feat: add flat routes lib

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,5 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { flatRoutes } from "@react-router/fs-routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+import { type RouteConfig } from "@react-router/dev/routes";
+
+export default flatRoutes() satisfies RouteConfig;
