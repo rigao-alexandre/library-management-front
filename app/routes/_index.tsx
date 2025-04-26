edbf2d0daeb11f659570c5c91ee7fd101c7ecaf8
Alexandre Paes Rigão <rigao.alexandre@gmail.com>
2025-04-26 00:48:12 -0300

    feat: add index rout

---
+++ app/routes/_index.tsx
@@ -0,0 +1,10 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+
+export default function Index() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/home");
+  }, []);
+}
