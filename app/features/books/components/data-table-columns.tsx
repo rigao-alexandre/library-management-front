02f625bc961742253fb6fd863a0eb6dae1d65609
Alexandre Paes Rigão <rigao.alexandre@gmail.com>
2025-04-28 17:04:04 -0300

    feat: add table componentes

---
+++ app/features/books/components/data-table-columns.tsx
@@ -0,0 +1,70 @@
+import { type ColumnDef } from "@tanstack/react-table";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MoreHorizontal } from "lucide-react";
+import { Link } from "react-router";
+
+export type Book = {
+  id: number;
+  title: string;
+  author: string;
+  isbn: string;
+  description: string;
+};
+
+export const columns: ColumnDef<Book>[] = [
+  {
+    id: "id",
+    header: "#",
+    cell: ({ row }) => {
+      const book = row.original;
+
+      return <Link to={`/books/${book.id}`}>{book.id}</Link>;
+    },
+  },
+  {
+    accessorKey: "title",
+    header: "Title",
+  },
+  {
+    accessorKey: "author",
+    header: "Author",
+  },
+  {
+    accessorKey: "isbn",
+    header: "ISBN",
+  },
+  {
+    accessorKey: "description",
+    header: "Description",
+  },
+  // {
+  //   id: "actions",
+  //   cell: ({ row }) => {
+  //     const book = row.original;
+
+  //     return (
+  //       <DropdownMenu modal={false}>
+  //         <DropdownMenuTrigger asChild>
+  //           {/* <MoreHorizontal className="h-4 w-4" /> */}
+  //           <Button variant="ghost" className="h-8 w-8 p-0">
+  //             <span className="sr-only">Open menu</span>
+  //             <MoreHorizontal className="h-4 w-4" />
+  //           </Button>
+  //         </DropdownMenuTrigger>
+  //         <DropdownMenuContent align="end">
+  //           <DropdownMenuItem>View customer</DropdownMenuItem>
+  //           <DropdownMenuItem>View payment details</DropdownMenuItem>
+  //         </DropdownMenuContent>
+  //       </DropdownMenu>
+  //     );
+  //   },
+  // },
+];
