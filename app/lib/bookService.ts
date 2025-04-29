69d49c653569532db52f97911435068d2cf8cfff
Alexandre Paes Rigão <rigao.alexandre@gmail.com>
2025-04-29 10:43:15 -0300

    feat: add API calls

--- app/lib/bookService.ts
+++ app/lib/bookService.ts
@@ -1,6 +1,14 @@
-import { type BookFormSchema, BookSchema } from "@/features/books/model/book";
+import {
+  BookCheckoutSchema,
+  type BookFormSchema,
+  BookSchema,
+} from "@/features/books/model/book";
 import { APIBaseService } from "./apiService";
 import { z } from "zod";
+import {
+  HistorySchema,
+  HistoryWithRelated,
+} from "@/features/history/model/history";
 
 export class BookService extends APIBaseService {
   constructor() {
@@ -25,5 +33,57 @@ export class BookService extends APIBaseService {
 
   async delete(id: number) {
     return super._delete(id);
+  }
+
+  async getAllHistory(id: number) {
+    const responseData = await this.makeRequest(
+      `/${this.model}/${id}/history`,
+      {
+        method: "GET",
+      }
+    );
+
+    const parsedResult = z.array(HistoryWithRelated).safeParse(responseData);
+
+    if (parsedResult.success) {
+      return parsedResult.data;
+    }
+
+    throw new Error(`Unexpected response shape`);
+  }
+
+  async checkOut(id: number, data: BookCheckoutSchema) {
+    const responseData = await this.makeRequest(
+      `/${this.model}/${id}/check-out`,
+      {
+        method: "POST",
+        body: JSON.stringify(data),
+      }
+    );
+
+    const parsedResult = HistorySchema.safeParse(responseData);
+
+    if (parsedResult.success) {
+      return parsedResult.data;
+    }
+
+    throw new Error(`Unexpected response shape`);
+  }
+
+  async checkIn(id: number) {
+    const responseData = await this.makeRequest(
+      `/${this.model}/${id}/check-in`,
+      {
+        method: "POST",
+      }
+    );
+
+    const parsedResult = HistorySchema.safeParse(responseData);
+
+    if (parsedResult.success) {
+      return parsedResult.data;
+    }
+
+    throw new Error(`Unexpected response shape`);
   }
 }
